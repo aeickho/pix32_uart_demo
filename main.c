bc3d7e1f12d12d9be0de49ae09f7d0d68f485796
alex <alex@artbanause.de>
2012-08-16 00:01:36 +0200

    mit irq geht

--- main.c
+++ main.c
@@ -102,9 +102,12 @@ FromUART2Fifo_in (void)
       UART2Fifo.in_read_pos = (UART2Fifo.in_read_pos + 1) % UART2Fifo.bufsize;
       UART2Fifo.in_nchar--;
     }
+  return (in);
 }
 
-int
+
+
+__inline__ int
 FromUART2Fifo_out (void)
 {
   int in = -1;
@@ -115,28 +118,21 @@ FromUART2Fifo_out (void)
 	(UART2Fifo.out_read_pos + 1) % UART2Fifo.bufsize;
       UART2Fifo.out_nchar--;
     }
+  return (in);
 }
 
+void
 UART2SendTrigger (void)
 {
-  int val;
-
-  if (send == 0)
+//  if (send == 0)
     {
-      val = FromUART2Fifo_out ();
-      if (val > 0)
-	{
-	  while (!UARTTransmitterIsReady (UART2));	// darf eigendlich nie anliegen
+      INTEnable (INT_SOURCE_UART_TX (UART2), INT_ENABLED);
 
-	  UARTSendDataByte (UART2, 'X');
-	  mLED_2_On ();
-
-	  send = 1;
-//mLED_2_Off();
-	}
+      send = 1;
     }
 }
 
+void
 UART2Send (const char *buffer, UINT32 size)
 {
   while (size)
@@ -148,12 +144,14 @@ UART2Send (const char *buffer, UINT32 size)
   UART2SendTrigger ();
 }
 
+void
 UART2SendChar (const char character)
 {
   ToUART2Fifo_out (character);
   UART2SendTrigger ();
 }
 
+void
 UART2PutStr (const char *buffer)
 {
   UART2Send (buffer, strlen (buffer));
@@ -175,19 +173,25 @@ IntUart2Handler (void)
   // We don't care about TX interrupt
   if (INTGetFlag (INT_SOURCE_UART_TX (UART2)))
     {
-      int val;
+      int val = -1;
+
+
       val = FromUART2Fifo_out ();
+
+
       if (val > 0)
 	{
 	  while (!UARTTransmitterIsReady (UART2));	// darf eigendlich nie anliegen
+
 	  UARTSendDataByte (UART2, val);
+	  INTClearFlag (INT_SOURCE_UART_TX (UART2));
 	  send = 1;
 	}
       else
 	{
 	  send = 0;
+	  INTEnable (INT_SOURCE_UART_TX (UART2), INT_DISABLED);
 	}
-      INTClearFlag (INT_SOURCE_UART_TX (UART2));
     }
 }
 
@@ -195,35 +199,37 @@ void
 SendDataBuffer (const char *buffer, UINT32 size)
 {
   while (size)
-      {
-            while (!UARTTransmitterIsReady (UART2))
-              ;
-              
-                    UARTSendDataByte (UART2, *buffer);
-                    
-                          mPORTAToggleBits (BIT_10);
-                          
-                                buffer++;
-                                      size--;
-                                          }
-                                          
-                                            while (!UARTTransmissionHasCompleted (UART2))
-                                                ;
-                                                }
-                                                
-                                                
+    {
+      while (!UARTTransmitterIsReady (UART2))
+	;
+
+      UARTSendDataByte (UART2, *buffer);
+
+      mPORTAToggleBits (BIT_10);
+
+      buffer++;
+      size--;
+    }
+
+  while (!UARTTransmissionHasCompleted (UART2))
+    ;
+}
+
+
 
 int
 main (void)
 {
-  int c;
-  char outBuf[10];
+
+  UART2FifoInit ();
+
+
   /* Configure PB frequency and wait states */
   SYSTEMConfigPerformance (40000000L);
   RPC9R = 2;
   U2RXR = 6;
-  TRISCbits.TRISC8 = 1;
-  TRISCbits.TRISC9 = 0;
+//  TRISCbits.TRISC8 = 1;
+//  TRISCbits.TRISC9 = 0;
   UARTConfigure (UART2, UART_ENABLE_PINS_TX_RX_ONLY | UART_ENABLE_HIGH_SPEED);
   UARTSetFifoMode (UART2,
 		   UART_INTERRUPT_ON_TX_NOT_FULL |
@@ -233,9 +239,10 @@ main (void)
 		      UART_STOP_BITS_1);
   UARTSetDataRate (UART2, GetPeripheralClock (), 500000);
   UARTEnable (UART2, UART_ENABLE_FLAGS (UART_PERIPHERAL | UART_RX | UART_TX));
-/*
+
   // Configure UART2 RX Interrupt
   INTEnable (INT_SOURCE_UART_RX (UART2), INT_ENABLED);
+//  INTEnable (INT_SOURCE_UART_TX (UART2), INT_ENABLED);
   INTSetVectorPriority (INT_VECTOR_UART (UART2), INT_PRIORITY_LEVEL_2);
   INTSetVectorSubPriority (INT_VECTOR_UART (UART2), INT_SUB_PRIORITY_LEVEL_0);
   // configure for multi-vectored mode
@@ -247,13 +254,43 @@ main (void)
   LATBbits.LATB15 = 0;
   TRISBbits.TRISB15 = 0;
 //  mPORTASetPinsDigitalOut (BIT_10);
-  UART2FifoInit ();
-*/
-while(1)
-  SendDataBuffer (mainMenu, sizeof (mainMenu));
-  
+
+
+
+#define DELAY 2156
+  T1CON = 0x8030;
+  PR1 = 0xffff;
+  TMR1 = 0;
+  while (TMR1 < DELAY);
+
+
+  mLED_2_On ();
 
   UART2PutStr ("Hallo NOKLAB\r\n");
+
+#define DELAYA 22156
+  T1CON = 0x8030;
+  PR1 = 0xffff;
+  TMR1 = 0;
+  while (TMR1 < DELAYA);
+
+
+  UART2PutStr ("1111111111111111\r\n");
+  UART2PutStr ("2222222222222222\r\n");
+  UART2PutStr ("3333333333333333\r\n");
+  UART2PutStr ("4444444444444444\r\n");
+  UART2PutStr ("5555555555555555\r\n");
+
+  T1CON = 0x8030;
+    PR1 = 0xffff;
+      TMR1 = 0;
+        while (TMR1 < DELAYA);
+        
+        
+  UART2PutStr ("6666666666666666\r\n");
+  UART2PutStr ("77777777777777777\r\n");
+  UART2PutStr ("888888888888888\r\n");
+
 
 
   while (1)
