65bffd483137a9e7aa508ddf21e135c2a7ccf3c9
tuxx42 <joelthomas@bdn.de>
2012-08-22 23:26:32 +0200

    uart.h: removed old prototypes

--- uart.h
+++ uart.h
@@ -1,7 +1,12 @@
 #ifndef HAVE_UART_H
 #define HAVE_UART_H
 
+#define SystemClock()		(40000000ul)
+#define GetPeripheralClock()	(SystemClock()/(1 << OSCCONbits.PBDIV))
+#define GetInstructionClock()	(SystemClock())
+
 #define UART_BLOCKING 1
+
 struct uart_buf {
 	char *buffer;
 	int read_pos;
@@ -16,25 +21,17 @@ struct uart_fifo {
 	int block_mode;
 };
 
-#define SystemClock()		(40000000ul)
-#define GetPeripheralClock()	(SystemClock()/(1 << OSCCONbits.PBDIV))
-#define GetInstructionClock()	(SystemClock())
+int uart_init(UART_MODULE uart);
 
-void uart_fifo_init(struct uart_fifo *fifo, int fifo_in_size, int fifo_out_size, int block_mode);
+void uart_fifo_init(struct uart_fifo *fifo, int fifo_in_size,
+		int fifo_out_size, int block_mode);
+int uart_poll(void);
+
 void uart_fifo_push(struct uart_buf *fifo, char ch);
 int uart_fifo_pop(struct uart_buf *fifo, char *ch);
+
 void uart2_send(char *buffer, UINT32 size);
 void uart2_print(const char *buffer);
-
-void ToUART2Fifo_in(const char character);
-void ToUART2Fifo_out(const char character);
-int FromUART2Fifo_in(void);
-inline int FromUART2Fifo_out(void);
-void UART2SendTrigger(void);
-void UART2PutStr(const char *buffer);
-void SendDataBuffer(const char *buffer, UINT32 size);
-int uart_init(UART_MODULE uart);
-int uart_poll(void);
 
 void uart_putchar(char c);
 
