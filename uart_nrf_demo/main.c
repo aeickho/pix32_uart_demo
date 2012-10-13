bbe1e87bb74dbfc14db5258a639839a6a7c51574
Alex <alex@artbanause.de>
2012-10-13 16:25:24 +0200

    geht immer noch

--- uart_nrf_demo/main.c
+++ uart_nrf_demo/main.c
@@ -109,25 +109,9 @@ main (void)
   UART2PutStr ("cannel: ,");
   UART2PutStr (outBuf);
   memcpy (config.mac0, "\x01\x02\x03\x02\x01", 5);
+  memcpy (config.txmac, "\x01\x02\x03\x02\x01", 5);
   nrf_config_set (&config);
   UART2PutStr ("done\n\r");
-
-  struct NRF_CFG oldconfig;
-
-  struct NRF_CFG config1 = {
-    .channel = 81,
-    .txmac = "\x1\x2\x3\x2\x1",
-    .nrmacs = 1,
-    .mac0 = "\x1\x2\x3\x2\x1",
-    .maclen = "\x20",
-  };
-
-  nrf_config_get (&oldconfig);
-  UART2PutStr ("config_get_done\n\r");
-
-  nrf_config_set (&config1);
-  UART2PutStr ("config_set_done\n\r");
-
 
   buf[0] = 32;
 
