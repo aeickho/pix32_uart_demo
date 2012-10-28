474d16fd645ea20fb98fbe4cffdf10e705717328
Alex <alex@artbanause.de>
2012-10-29 00:10:23 +0100

    mal wieder

--- uart_nrf_demo_rx/host/main.c
+++ uart_nrf_demo_rx/host/main.c
@@ -17,19 +17,13 @@
 
 #define UART1 "/dev/serial/by-id/usb-Silicon_Labs_CP2102-Alex_0101-if00-port0"
 
-
 #define	STEP_INIT 		0
 #define STEP_WAIT		1
 #define STEP_READ		2
 #define STEP_RX_NEWFRAME	3
 #define STEP_END		-1
 
-#define WORDS_PER_FRAGMENT   6
-#define BYTES_PER_FRAGMENT  (WORDS_PER_FRAGMENT*4)
-
-#define FRAMEBUFSIZE		20
-#define MAX_MSG_FRAME_NR	10
-
+#define FRAMEBUFSIZE		15
 
 #define WORDS_PER_FRAGMENT   6
 #define BYTES_PER_FRAGMENT  (WORDS_PER_FRAGMENT*4)
@@ -37,7 +31,6 @@
 #define MAX_RECOV_FRAGMENTS  3
 #define MAX_MESSAGE_SIZE    (BYTES_PER_FRAGMENT*MAX_DATA_FRAGMENTS)
 #define FRAME_BUFF_SIZE     (MAX_DATA_FRAGMENTS+MAX_RECOV_FRAGMENTS)
-
 
 #define NUM_DATA_FRAGS(pf)  ((pf)->metad >> 12)
 #define FRAGMENT_INDEX(pf)  ((pf)->metad &  63)
@@ -65,18 +58,18 @@ struct mid_processeds
   int cnt;
 };
 
+#define MID_PROCESSED_SIZE 5
 struct sframe frameBuffer[FRAMEBUFSIZE];	// 800 
-struct mid_processeds mid_processed[FRAMEBUFSIZE];
+struct mid_processeds mid_processed[MID_PROCESSED_SIZE];
 int mid_processed_wp;
 
 int
 main (int argc, char **argv)
 {
   int tty;
-  int i;
   unsigned char buf[BUFSIZE];
-  unsigned char outBuf[BUFSIZE];
-          unsigned short c_crc16, r_crc16;
+  unsigned char inData[BUFSIZE];
+  unsigned short c_crc16, r_crc16;
 
   int step;
   FILE *rawfile;
@@ -90,7 +83,6 @@ main (int argc, char **argv)
       perror ("opening TTY....");
       exit (2);
     }
-
 
   rawfile = fopen ("raw.txt", "w");
   if (rawfile == NULL)
@@ -119,27 +111,28 @@ main (int argc, char **argv)
 	    step = STEP_READ;
 	  break;
 	case STEP_READ:
-
 	  read (tty, buf, 8);
-	  from_base128n (buf, outBuf, 1);
-	  seq_nr = *(unsigned int *) outBuf;
+	  from_base128n (buf, inData, 1);
+	  seq_nr = *(unsigned int *) inData;
 	  read (tty, buf, 40);
-	  from_base128n (buf, outBuf, 5);
+	  from_base128n (buf, inData, 5);
 
-	  r_crc16 = outBuf[31] << 8 | outBuf[30];
-	  c_crc16 = crc16 (outBuf, 30);
+	  r_crc16 = inData[31] << 8 | inData[30];
+	  c_crc16 = crc16 (inData, 30);
 
 	  if (r_crc16 == c_crc16)
 	    {
-
+             // ret = number of char
+	     // ret = process_frame(inData,outData)
+	     
 	      int flag_ng = 0;
-	      struct frame new_frame;
+	      struct frame *new_frame;
 
-	      memcpy (&new_frame, outBuf, 32);
+	      new_frame = (struct frame *) inData;
 
-	      for (i = 0; i < FRAMEBUFSIZE; i++)
+	      for (int i = 0; i < MID_PROCESSED_SIZE; i++)
 		{
-		  if (mid_processed[i].mid == new_frame.mid)
+		  if (mid_processed[i].mid == new_frame->mid)
 		    {
 		      if (mid_processed[i].cnt == -1)
 			{
@@ -148,7 +141,8 @@ main (int argc, char **argv)
 			}
 		      mid_processed[i].cnt++;
 		      flag_ng = -2;
-		      if (mid_processed[i].cnt > 6)
+
+		      if (mid_processed[i].cnt > NUM_DATA_FRAGS (new_frame))
 			{
 			  flag_ng = -3;
 			  mid_processed[i].cnt = -1;
@@ -156,24 +150,23 @@ main (int argc, char **argv)
 		    }
 		}
 
+	      // Nachricht mit dieser MID  schon verarbeitet    
 	      if (flag_ng == -1)
 		{
 		  step = STEP_WAIT;
 		  break;
 		}
-
+	      // neue Message ID (in mid_processed_wp eintagen)         
 	      if (flag_ng == 0)
 		{
-		  mid_processed[mid_processed_wp].mid = new_frame.mid;
+		  mid_processed[mid_processed_wp].mid = new_frame->mid;
 		  mid_processed[mid_processed_wp].cnt = 1;
 		  mid_processed_wp++;
-		  mid_processed_wp %= FRAMEBUFSIZE;
+		  mid_processed_wp %= MID_PROCESSED_SIZE;
 		}
-
-	      int seq_nr_diff_min = 0;
+	      // suche ältesten eintrag im FrameBuffer 
 	      int seq_nr_id;
-
-	      for (i = 0; i < FRAMEBUFSIZE; i++)
+	      for (int seq_nr_diff_min = 0, i = 0; i < FRAMEBUFSIZE; i++)
 		{
 		  if (seq_nr - frameBuffer[i].seqnr > seq_nr_diff_min)
 		    {
@@ -182,7 +175,7 @@ main (int argc, char **argv)
 		    }
 		}
 
-	      memcpy (&frameBuffer[seq_nr_id].frame, outBuf, 32);
+	      memcpy (&frameBuffer[seq_nr_id].frame, new_frame, 32);
 	      frameBuffer[seq_nr_id].seqnr = seq_nr;
 
 	      if (flag_ng == -3)
@@ -196,24 +189,27 @@ main (int argc, char **argv)
 
 		  memset (tx_valid, 0, sizeof (tx_valid));
 		  memset (fragdatas, 0, nr_data_frames + 3);
-
-		  for (i = 0; i < FRAMEBUFSIZE; i++)
+		  // create pointerlist
+		  for (int i = 0; i < FRAMEBUFSIZE; i++)
 		    {
-		      if (frameBuffer[i].frame.mid == new_frame.mid)
+		      if (frameBuffer[i].frame.mid == new_frame->mid)
 			{
-			  tx_valid[FRAGMENT_INDEX (&frameBuffer[i].frame)] =
-			    1;
-			  fragdatas[FRAGMENT_INDEX (&frameBuffer[i].frame)]
+			  tx_valid[FRAGMENT_INDEX
+				   (&frameBuffer[i].frame)] = 1;
+			  fragdatas[FRAGMENT_INDEX
+				    (&frameBuffer
+				     [i].frame)]
 			    = frameBuffer[i].frame.fragmentdata;
 			  nr_data_frames =
 			    (NUM_DATA_FRAGS (&frameBuffer[i].frame));
-			  if ((FRAGMENT_INDEX (&frameBuffer[i].frame)) >
-			      tx_valid_max)
+			  if ((FRAGMENT_INDEX
+			       (&frameBuffer[i].frame)) > tx_valid_max)
 			    tx_valid_max =
 			      FRAGMENT_INDEX (&frameBuffer[i].frame);
 			}
 		    }
 
+		  // generate spare space for redundance   
 		  for (int ii = 0, i = 0; i < nr_data_frames + 3; i++)
 		    {
 		      if (tx_valid[i] == 0)
@@ -222,11 +218,12 @@ main (int argc, char **argv)
 			}
 		    }
 
-		  tfec3_decode (WORDS_PER_FRAGMENT, nr_data_frames,
-				tx_valid_max - nr_data_frames + 1, tx_valid,
-				fragdatas);
+		  tfec3_decode (WORDS_PER_FRAGMENT,
+				nr_data_frames,
+				tx_valid_max -
+				nr_data_frames + 1, tx_valid, fragdatas);
 
-		  for (i = 0; i < nr_data_frames; i++)
+		  for (int i = 0; i < nr_data_frames; i++)
 		    {
 		      int ii;
 		      char **p;
