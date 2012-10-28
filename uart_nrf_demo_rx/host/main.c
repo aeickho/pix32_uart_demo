c0e9ba13b7ab78a605a3e0b7931954b3d2aef953
Alex <alex@artbanause.de>
2012-10-28 18:14:55 +0100

    nur so

--- uart_nrf_demo_rx/host/main.c
+++ uart_nrf_demo_rx/host/main.c
@@ -28,6 +28,16 @@
 #define FRAMEBUFSIZE		20
 #define MAX_MSG_FRAME_NR	10
 
+
+#define WORDS_PER_FRAGMENT   6
+#define BYTES_PER_FRAGMENT  (WORDS_PER_FRAGMENT*4)
+#define MAX_DATA_FRAGMENTS  15
+#define MAX_RECOV_FRAGMENTS  3
+#define MAX_MESSAGE_SIZE    (BYTES_PER_FRAGMENT*MAX_DATA_FRAGMENTS)
+#define FRAME_BUFF_SIZE     (MAX_DATA_FRAGMENTS+MAX_RECOV_FRAGMENTS)
+
+
+
 //#if UINT_MAX >= 0xFFFFFFFF
 typedef unsigned int tfec3_u32;	/* let's prefer int if wide enough */
 //#else
@@ -59,7 +69,21 @@ struct msgindex
   unsigned char used;
 };
 
+
+
 struct sframe frameBuffer[FRAMEBUFSIZE];	// 800 
+
+struct sframe frameBufferNG[FRAMEBUFSIZE];
+
+struct mid_processeds
+{
+  tfec3_u32 mid;
+  int cnt;
+};
+
+struct mid_processeds mid_processed[FRAMEBUFSIZE];
+int mid_processed_wp;
+
 
 
 static void
@@ -94,8 +118,6 @@ main (int argc, char **argv)
   int delete_msg_id_idx = 0;
   FILE *rawfile;
 
-
-
   unsigned int seq_nr;
   struct termios termOptions;
 
@@ -105,6 +127,17 @@ main (int argc, char **argv)
       frameBuffer[i].seqnr = 0;
       delete_msg_id[i] = 0;
     }
+
+
+  for (i = 0; i < FRAMEBUFSIZE; i++)
+    {
+      frameBufferNG[i].timeout = 0;
+      frameBufferNG[i].seqnr = 0;
+      mid_processed[i].cnt = 0;
+    }
+
+  mid_processed_wp = 0;
+
 
 
   tty = open (UART1, O_RDWR | O_NOCTTY);
@@ -150,16 +183,133 @@ main (int argc, char **argv)
 
 	  r_crc16 = outBuf[31] << 8 | outBuf[30];
 	  c_crc16 = crc16 (outBuf, 30);
-	  
-	  printf ( "%d :", seq_nr);
-	  
+
+	  printf ("%d :", seq_nr);
+
 
 	  for (i = 0; i < 32; i++)
-	    printf ( "%02x ", outBuf[i]);
+	    printf ("%02x ", outBuf[i]);
 
-	  printf ( "%04x %s\n", c_crc16,
-		   r_crc16 == c_crc16 ? "ok" : "nok");
+	  printf ("%04x %s\n", c_crc16, r_crc16 == c_crc16 ? "ok" : "nok");
 
+
+	  if (r_crc16 == c_crc16)
+	    {
+
+	      int flag_ng = 0;
+	      struct frame new_frame_ng;
+
+	      memcpy (&new_frame_ng, outBuf, 32);
+
+	      printf ("%x \n", new_frame_ng.mid);
+	      // check for: if mid processed
+	      for (i = 0; i < FRAMEBUFSIZE; i++)
+		{
+		  if (mid_processed[i].mid == new_frame_ng.mid)
+		    {
+		      if (mid_processed[i].cnt == -1)
+			{
+			  flag_ng = -1;
+			  break;
+			}
+		      mid_processed[i].cnt++;
+		      flag_ng = -2;
+		      if (mid_processed[i].cnt > 6)
+			{
+			  flag_ng = -3;
+			  mid_processed[i].cnt = -1;
+			}
+		    }
+		}
+
+	      if (flag_ng == -1)
+		{
+		  goto a;
+		  step = STEP_WAIT;
+		  break;
+		}
+
+	      if (flag_ng == 0)
+		{
+		  mid_processed[mid_processed_wp].mid = new_frame_ng.mid;
+		  mid_processed[mid_processed_wp].cnt = 1;
+		  mid_processed_wp++;
+		  mid_processed_wp %= FRAMEBUFSIZE;
+		}
+
+	      int seq_nr_diff_min = 0;
+	      int seq_nr_id;
+
+	      for (i = 0; i < FRAMEBUFSIZE; i++)
+		{
+		  if (seq_nr - frameBufferNG[i].seqnr > seq_nr_diff_min)
+		    {
+		      seq_nr_diff_min = seq_nr - frameBufferNG[i].seqnr;
+		      seq_nr_id = i;
+		    }
+		}
+
+	      memcpy (&frameBufferNG[seq_nr_id].frame, outBuf, 32);
+	      frameBufferNG[seq_nr_id].seqnr = seq_nr;
+
+	      if (flag_ng == -3)
+		{
+		  unsigned char tx_valid[FRAME_BUFF_SIZE];
+		  tfec3_u32 *fragdatas[FRAME_BUFF_SIZE];
+		  char nr_data_frames;
+		  struct frame spare_frame[3];
+		  memset (tx_valid, 0, sizeof (tx_valid));
+
+		  for (i = 0; i < FRAMEBUFSIZE; i++)
+		    {
+		      if (frameBufferNG[i].frame.mid == new_frame_ng.mid)
+			{
+			  tx_valid[i] = 1;
+			  fragdatas[i] = frameBufferNG[i].frame.fragmentdata;
+			  nr_data_frames =
+			    (frameBufferNG[i].frame.metad & 0xf000) >> 12;
+			}
+		    }
+
+
+
+
+		  printf ("----------> %d\n", nr_data_frames);
+
+		  int ii = 0;
+		  for (i = 0; i < nr_data_frames + 3;i++)
+		    {
+		      if (tx_valid[i] == 0)
+			{
+			  fragdatas[i] = spare_fragmentdata[ii++];
+			}
+		    }
+		  for (i = 0; i < nr_data_frames + 3;i++)
+		    {
+		      printf ("%p \n", fragdatas[i]);
+		    }
+
+
+
+
+		  for (i = 0; i < FRAMEBUFSIZE; i++)
+		    {
+		      for (int ii = 0; ii < FRAMEBUFSIZE; ii++)
+			{
+			  if (frameBufferNG[ii].frame.mid == new_frame_ng.mid
+			      && i == (frameBufferNG[ii].frame.metad & 0x1f))
+			    {
+			      printf ("%d %d %x %d %d\n", i, ii,
+				      frameBufferNG[ii].frame.mid,
+				      frameBufferNG[ii].seqnr,
+				      frameBufferNG[ii].frame.metad & 0x1f);
+			    }
+			}
+		    }
+		}
+
+	    }
+	a:
 	  if (r_crc16 == c_crc16)
 	    {
 	      int seq_nr_id;
@@ -245,7 +395,7 @@ main (int argc, char **argv)
 		    }
 		}
 	    }
-	    
+
 	  step = STEP_WAIT;
 	  break;
 	}
